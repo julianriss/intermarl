bfa06ad6c8f91fac096238ca438fbbce30644aae
ge69hoj <f.pieroth@gmx.net>
2022-04-20 17:50:18 +0200

    Delete redundant stuff

--- pickle_viewer.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import sys
-
-if __name__ == "__main__":
-    argv = sys.argv
-    if len(argv) <= 1:
-        print("Specify pickle file as parameter.")
-    else:
-        print(pickle.load(open(argv[1], "rb")))
